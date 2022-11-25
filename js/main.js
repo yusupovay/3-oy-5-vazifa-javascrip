f479fecb4a185c9d7683303f85a480cb8dd3d222
qunduz2104 <yusupova21042001@gmail.com>
2022-11-25 14:33:06 +0500

    5-vazifa

---
+++ js/main.js
@@ -0,0 +1,80 @@
+var userName = prompt("Ismingizni kiriting?");
+console.log(userName);
+// 1
+var test1 = prompt(
+  "Tartibli ro'yhat tegi qaysi" +
+    "\n" +
+    "a)ul" +
+    "\n" +
+    "b)ol" +
+    "\n" +
+    "c)dt" +
+    "\n" +
+    "d)dd"
+);
+console.log("1-test" + "\n" + "Javobim:" + test1 + "\n" + "To'g'ri javob:b");
+// 2
+var test2 = prompt(
+  "Textlardagi so'zlar orasidagi masofani o'zgartirish uchun qanday stil kerak" +
+    "\n" +
+    "a)letter_spesing" +
+    "\n" +
+    "b)word-specing" +
+    "\n" +
+    "c)word:specing" +
+    "\n" +
+    "d)letter-spesing"
+);
+console.log("2-test" + "\n" + "Javobim:" + test2 + "\n" + "To'g'ri javob:b");
+//   3
+var test3 = prompt(
+  "Inputda kamida nechta belgi ishlatishimizni qaysi atribut bilan chegaralaymiz" +
+    "\n" +
+    "a)min=''" +
+    "\n" +
+    "b)max=''" +
+    "\n" +
+    "c)minlength=''" +
+    "\n" +
+    "d)maxlenth=''"
+);
+console.log("3-test" + "\n" + "Javobim:" + test3 + "\n" + "To'g'ri javob:c");
+// 4
+var test4 = prompt(
+  "Cssda ranglarini gradus orqali berib bo'ladim?" +
+    "\n" +
+    "a)Yo'q" +
+    "\n" +
+    "b)Bilmayman " +
+    "\n" +
+    "c)Hamma javob to'gri" +
+    "\n" +
+    "d)ha"
+);
+console.log("4-test" + "\n" + "Javobim:" + test4 + "\n" + "To'g'ri javob:d");
+// 5
+var test5 = prompt(
+  "beckground-repeat: ga qaysi stilni bersak  to'liq bo'lib takrorlanadi" +
+    "\n" +
+    "a)repeat" +
+    "\n" +
+    "b)no-repeat" +
+    "\n" +
+    "c)space" +
+    "\n" +
+    "d)round"
+);
+console.log("5-test" + "\n" + "Javobim:" + test5 + "\n" + "To'g'ri javob:d");
+//   6
+var test6 = confirm(
+  "Formda  action='' atributiga src bermasak beckend ga ketmaydi " +
+    "\n" +
+    "a)true" +
+    "\n" +
+    "b)false" +
+    "\n" +
+    "c)Hammasi to'gri" +
+    "\n" +
+    "d)Xato"
+);
+console.log("6-test" + "\n" + "Javobim:" + test6 + "\n" + "To'g'ri javob:true");
